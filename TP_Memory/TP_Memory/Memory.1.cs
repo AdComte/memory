af1813c1a4fd32ef421112d2615c0032da1cf5ce
AdComte <adrien.comte.69@gmail.com>
2014-12-16 15:41:33 +0100

    renommage

--- TP_Memory/TP_Memory/Memory.1.cs
+++ TP_Memory/TP_Memory/Memory.1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dllLoto;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,9 +7,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
-using System.Windows.Forms;
-using dllLoto;
 
 
 namespace TP_Memory
@@ -35,7 +33,6 @@ namespace TP_Memory
 
             // On effectue la distribution (aléatoire) proprement dite
             Distribution_Aleatoire();
-
         }
 
         private void Distribution_Aleatoire()
--- TP_Memory/TP_Memory/Memory.2.cs
+++ TP_Memory/TP_Memory/Memory.2.cs
@@ -1,21 +1,23 @@
-﻿
-using dllLoto;
+﻿using dllLoto;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
 
-
 namespace TP_Memory
 {
-    public partial class frmMemory : Form
+    public partial class Memory : Form
     {
         // Déclaration des variables globales du jeu
-        int nbCartesDansSabot;  // Nombre de cartes dans le sabot (en fait nombre
-        // d'images dans le réservoir)
+
+        int nbCartesDansSabot;  // Nombre de cartes dans le sabot (en fait nombre d'images dans le réservoir)
         int nbCartesSurTapis;   // Nombre de cartes sur le tapis
+
 
         private void Distribution_Aleatoire()
         {
@@ -32,12 +34,13 @@ namespace TP_Memory
             int i_image;
             for (int i_carte = 0; i_carte < nbCartesSurTapis; i_carte++)
             {
-                carte = (PictureBox)tlpTapisDeCartes.Controls[i_carte];
+                carte = (PictureBox) tlpTapisDeCartes.Controls[i_carte];
                 i_image = tImagesCartes[i_carte + 1]; // i_carte + 1 à cause
                 // des pbs d'indices
                 carte.Image = ilSabotDeCartes.Images[i_image];
             }
         }
+
 
 
 
