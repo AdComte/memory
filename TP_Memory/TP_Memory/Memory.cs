a0480e0272cea959b54b2cb938de5c65663047dd
AdComte <adrien.comte.69@gmail.com>
2014-12-31 16:01:12 +0100

    Correction des doubles définition de la méthode Distribution_Aleatorie et des attributs entiers et de leur properties

--- TP_Memory/TP_Memory/Memory.cs
+++ TP_Memory/TP_Memory/Memory.cs
@@ -13,6 +13,37 @@ namespace TP_Memory
 {
     public partial class Memory : Form
     {
+
+        private int nbCartesDansSabot;  // Nombre de cartes dans le sabot (en fait nombre d'images dans le réservoir)
+        private int nbCartesSurTapis;   // Nombre de cartes sur le tapis
+
+
+        public int NbCartesDansSabot
+        {
+            get
+            {
+                return nbCartesDansSabot;
+            }
+            set
+            {
+                nbCartesDansSabot = value;
+            }
+
+        }
+
+        public int NbCartesSurTapis
+        {
+            get
+            {
+                return nbCartesSurTapis;
+            }
+            set
+            {
+                nbCartesSurTapis = value;
+            }
+        }
+
+
         public Memory()
         {
             InitializeComponent();
@@ -35,7 +66,7 @@ namespace TP_Memory
             Distribution_Aleatoire();
         }
 
-        private void Distribution_Aleatoire()
+        private void Distribution_Aleatoire2()
         {
             throw new NotImplementedException();
         }
@@ -78,8 +109,30 @@ namespace TP_Memory
         }
 
 
-        public int nbCartesDansSabot { get; set; }
 
-        public int nbCartesSurTapis { get; set; }
+
+
+
+        private void Distribution_Aleatoire()
+        {
+            // On utilise la LotoMachine pour générer une série aléatoire
+            LotoMachine hasard = new LotoMachine(nbCartesDansSabot);
+            // On veut une série de nbCartesSurTapis cartes parmi celles 
+            // du réservoir
+            int[] tImagesCartes = hasard.TirageAleatoire(nbCartesSurTapis, false);
+            // La série d'entiers retournée par la LotoMachine correspondra
+            // aux indices des cartes dans le "sabot"
+
+            // Affectation des images aux picturebox
+            PictureBox carte;
+            int i_image;
+            for (int i_carte = 0; i_carte < nbCartesSurTapis; i_carte++)
+            {
+                carte = (PictureBox)tlpTapisDeCartes.Controls[i_carte];
+                i_image = tImagesCartes[i_carte + 1]; // i_carte + 1 à cause
+                // des pbs d'indices
+                carte.Image = ilSabotDeCartes.Images[i_image];
+            }
+        }
     }
 }
